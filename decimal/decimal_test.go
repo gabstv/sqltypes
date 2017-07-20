1a44ab4d3be57f45fefd80b06def6d02e6970935
Gabriel Ochsenhofer <gabs@gabs.tv>
2017-07-20 18:25:57 -0300

    decimal needs tests

--- decimal/decimal_test.go
+++ decimal/decimal_test.go
@@ -13,4 +13,14 @@ func TestBasics(t *testing.T) {
 	assert.Equal(t, d3.StringFixed(1), "3.0", "they should be equal")
 	d1 = d1.Mul(d3).Mul(d3)
 	assert.Equal(t, d1.StringFixed(1), "9.0", "they should be equal")
+	// json
+	d9 := New(434, -1)
+	bbb, _ := d9.MarshalJSON()
+	d4 := New(1, 0)
+	d4r := &d4
+	err := d4r.UnmarshalJSON(bbb)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, d4.StringFixed(2), "4.34", "they should be equal")
 }
